b3e7553f00f4e816c21e3b421b791428e4964da8
Andrew McDonald <andrewmcdonald.dev@gmail.com>
2024-09-30 19:30:21 -0400

    Massive upgrade. Properly parsing KV3/VDATA to JSON

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ You then have to put the executable within the same folder of the VPK decompiler
 ```
 $  ./vpkParser [location of targeted vpk file] [location of vdata inside the vpk]
 ```
+
+
+### WARNING 
+If you are trying to use this and this was not made for you this implementation is wack.
+
+Any line with `soundevent:`, `panorama:`, or `resource_name` was thrown out because it was too hard to parse stupidly.
+
+If the parser fails on any given line it will give up and put an empty string. 
+
+If that works for you use this for whatevs idgaf.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use std::process::Command;
 
 fn main() -> Result<(), String> {
@@ -28,15 +29,74 @@ fn grab_vdata(path_to_vpk: String, path_to_vdata: String) -> Result<String, Stri
 }
 
 fn parse_vdata_to_json(vdata: String) -> Result<String, String> {
-    let output: String = vdata
+    let assignment = Regex::new(
+        // r#"([a-zA-z_0-9]*) = (?:("[a-zA-Z0-9_+\.\| -#]*"|true|false|[+-]?[\d]*\.?\d+|))"#
+        r#"([a-zA-z_0-9]*) = (?:("[^"]*"|true|false|[+-]?[\d]*\.?\d+|))"#
+    ).unwrap();
+    let braces = Regex::new(
+        r#"(\[|\]|\{|\})"#
+    ).unwrap();
+    let number_or_string = Regex::new(
+        r#"([+-]?[\d]*\.?\d+|"[a-zA-Z0-9_+\.\| ]*")"#
+    ).unwrap();
+    let bad_line_detector = Regex::new("soundevent:|panorama:|resource_name:").unwrap();
+
+    let json = vdata
         .split_once(r#"--- Data for block "DATA" ---"#)
         .ok_or("VDATA file was formatted incorrectly. Probably because path to VPK or path to VDATA is incorrect.")?
         .1
+        .chars()
+        .collect::<String>()
         .lines()
         .skip(2)
-        .map(|line| line.chars().filter(|c| c != &'\t').collect::<String>())
-        .map(|line| str::replace(&line.clone(), " = ", ":"))
-        .collect();
+        .map(|line| {
+            // Removes all lines with `soundevent:`, `panorama:`, or `resource_name`.
+            if bad_line_detector.captures(line).is_some() {
+                return "".to_string();
+            }
 
-    Ok(output)
+            if let Some((_, [var, assign])) = assignment.captures(line).map(|c| c.extract()) {
+                return format!("\"{}\":{},", var, assign);
+            }            
+
+            if let Some((_, [brace])) = braces.captures(line).map(|c| c.extract()) {
+                return match brace {
+                    "{" | "[" => brace.to_string(),
+                    "}" => "},".to_string(),
+                    "]" => "],".to_string(),
+                    // Should never reach here
+                    _ => "".to_string()
+                };
+            }
+
+            if let Some((_, [item])) = number_or_string.captures(line).map(|c| c.extract()) {
+                return format!("{},", item);
+            }
+
+            "".to_string()
+        })
+    .collect::<String>();
+
+    // Correct trailing commas.
+    let json = str::replace(&json, ",}", "}");
+    let json = str::replace(&json, ",]", "]");
+    // Clear edge case where variable is assigned to object
+    let json = str::replace(&json, ",[", "[");
+    let json = str::replace(&json, ",{", "{");
+
+    // Fixes edge case where `},{` gets changed to `}{` in arrays.
+    // Also for `],[` to `][`
+    let json = str::replace(&json, "}{", "},{");
+    let json = str::replace(&json, "][", "],[");
+
+    // Get rid of last trailing commas
+    let mut json = json.chars();
+    json.next_back();
+    let json = json.collect::<String>();
+
+    // UNCOMMENT WHEN TESTING
+    // This line 'fixes' all the shit i couldn't figure out. (Deletes Content)
+    let json = str::replace(&json, ":,", ":\"\",");
+
+    Ok(json)
 }
