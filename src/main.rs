7605344eaee026f868319f8fbe3d135ad30e17bf
Andrew McDonald <andrewmcdonald.dev@gmail.com>
2024-09-30 13:11:10 -0400

    Updated error checking

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() -> Result<(), String> {
     let path_to_vdata = std::env::args().nth(2).ok_or("Bad path to VDATA.\n Remember, correct formatting is vpkParser [path to vpk] [path to vdata]")?;
 
     let vdata = grab_vdata(path_to_vpk, path_to_vdata)?;
-    let json = parse_vdata_to_json(vdata);
+    let json = parse_vdata_to_json(vdata)?;
 
     println!("{}", json);
     Ok(())
@@ -20,19 +20,23 @@ fn grab_vdata(path_to_vpk: String, path_to_vdata: String) -> Result<String, Stri
         .arg("-b")
         .arg("DATA")
         .output()
-        .map_err(|_| "Decompiler failed to open VPK file.")?;
+        .map_err(|e| format!("Could not find Decompiler: {}", e))?;
 
+    // Unwrap here because whomever wrote the decompiler doesnt like errors and insteads likes bad
+    // outputs
     Ok(String::from_utf8(output.stdout).unwrap())
 }
 
-fn parse_vdata_to_json(vdata: String) -> String {
-    vdata
+fn parse_vdata_to_json(vdata: String) -> Result<String, String> {
+    let output: String = vdata
         .split_once(r#"--- Data for block "DATA" ---"#)
-        .unwrap()
+        .ok_or("VDATA file was formatted incorrectly. Probably because path to VPK or path to VDATA is incorrect.")?
         .1
         .lines()
         .skip(2)
         .map(|line| line.chars().filter(|c| c != &'\t').collect::<String>())
         .map(|line| str::replace(&line.clone(), " = ", ":"))
-        .collect::<String>()
+        .collect();
+
+    Ok(output)
 }
